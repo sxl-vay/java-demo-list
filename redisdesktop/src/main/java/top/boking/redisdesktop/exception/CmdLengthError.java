0b2847e1c512c5d5b7bfa86cde73b4cb49e1a589
mac <1@2.com>
2024-05-17 22:52:06 +0800

    redisdesktop清理

--- redisdesktop/src/main/java/top/boking/redisdesktop/exception/CmdLengthError.java
+++
@@ -1,8 +0,0 @@
-package top.boking.redisdesktop.exception;
-
-/**
- * @author shxl
- * @data 2022/6/2 23:52
- **/
-public class CmdLengthError extends Exception {
-}
--- sharding/src/main/java/top/boking/sharding/entity/Goods.java
+++ sharding/src/main/java/top/boking/sharding/entity/Goods.java
@@ -10,7 +10,6 @@ import lombok.NoArgsConstructor;
  **/
 @Data
 @AllArgsConstructor
-
 public class Goods {
     private Long orderId;
     private String name;
